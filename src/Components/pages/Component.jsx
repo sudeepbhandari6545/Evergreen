2b7ef2c89660e5d7453920cd52125d7505d4d205
sudeepbhandari6545 <sudeepbhandari6545@gmail.com>
2021-02-18 16:05:43 +0545

    evergreen

--- src/Components/pages/Component.jsx
+++ src/Components/pages/Component.jsx
@@ -24,29 +24,235 @@ const Component = () => {
         <h4>Foundation</h4>
         <p>Styles and primitive components. Start here.</p>
         <Row>
-          <Card className="m-3" style={{ width: '13rem', height: '5rem' }}>
-            <Card.Img
-              variant="top"
-              src={process.env.PUBLIC_URL + '/image/comimg1.jpg'}
-            />
-            <h5 className="text-center">Layout primitive</h5>
-          </Card>
-          <Card className="m-3" style={{ width: '13rem', height: '5rem' }}>
-            <Card.Img
-              variant="top"
-              src={process.env.PUBLIC_URL + '/image/comimg2.jpg'}
-            />
-            <h5 className="text-center">Layout primitive</h5>
-          </Card>
-          <Card className="m-3" style={{ width: '13rem', height: '5rem' }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <h5 className="text-center">Layout primitive</h5>
-          </Card>
-          <Card className="m-3" style={{ width: '13rem', height: '5rem' }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <h5 className="text-center">Layout primitive</h5>
-          </Card>
+          <Col md={3}>
+            <Card className="" style={{ width: '13rem', height: '12rem' }}>
+              <Card.Img
+                variant="center"
+                src={process.env.PUBLIC_URL + '/image/comimg1.jpg'}
+                style={{
+                  width: '13rem',
+                  height: '12rem',
+                  padding: '30px',
+                  backgroundColor: 'lightgray',
+                }}
+              />
+            </Card>
+
+            <p className="text-capitalize text-center pt-3">Layout primitive</p>
+          </Col>
+          <Col md={3}>
+            <Card className="" style={{ width: '13rem', height: '12rem' }}>
+              <Card.Img
+                variant="center"
+                src={process.env.PUBLIC_URL + '/image/comimg2.jpg'}
+                style={{
+                  width: '13rem',
+                  height: '12rem',
+                  padding: '30px',
+                  backgroundColor: 'lightgray',
+                }}
+              />
+            </Card>
+            <p className="text-capitalize text-center pt-3">Layout primitive</p>
+          </Col>
+          <Col md={3}>
+            <Card className="" style={{ width: '13rem', height: '12rem' }}>
+              <Card.Img
+                variant="center"
+                src={process.env.PUBLIC_URL + '/image/comimg1.jpg'}
+                style={{
+                  width: '13rem',
+                  height: '12rem',
+                  padding: '30px',
+                  backgroundColor: 'lightgray',
+                }}
+              />
+            </Card>
+            <p className="text-capitalize text-center pt-3">Layout primitive</p>
+          </Col>
+          <Col md={3}>
+            <Card className="" style={{ width: '13rem', height: '12rem' }}>
+              <Card.Img
+                variant="center"
+                src={process.env.PUBLIC_URL + '/image/comimg1.jpg'}
+                style={{
+                  width: '13rem',
+                  height: '12rem',
+                  padding: '30px',
+                  backgroundColor: 'lightgray',
+                }}
+              />
+            </Card>
+            <p className="text-capitalize text-center pt-3">Layout primitive</p>
+          </Col>
         </Row>
+      </div>
+      <div>
+        <div className="component-detail">
+          <h4>Components</h4>
+          <p>Buttons & Atomic Elements.</p>
+          <Row>
+            <Col md={3}>
+              <Card className="" style={{ width: '13rem', height: '12rem' }}>
+                <Card.Img
+                  variant="center"
+                  src={process.env.PUBLIC_URL + '/image/comimg1.jpg'}
+                  style={{
+                    width: '13rem',
+                    height: '12rem',
+                    padding: '30px',
+                    backgroundColor: 'lightgray',
+                  }}
+                />
+              </Card>
+
+              <p className="text-capitalize text-center pt-3">button</p>
+            </Col>
+            <Col md={3}>
+              <Card className="" style={{ width: '13rem', height: '12rem' }}>
+                <Card.Img
+                  variant="center"
+                  src={process.env.PUBLIC_URL + '/image/comimg2.jpg'}
+                  style={{
+                    width: '13rem',
+                    height: '12rem',
+                    padding: '30px',
+                    backgroundColor: 'lightgray',
+                  }}
+                />
+              </Card>
+              <p className="text-capitalize text-center pt-3">tab</p>
+            </Col>
+            <Col md={3}>
+              <Card className="" style={{ width: '13rem', height: '12rem' }}>
+                <Card.Img
+                  variant="center"
+                  src={process.env.PUBLIC_URL + '/image/comimg1.jpg'}
+                  style={{
+                    width: '13rem',
+                    height: '12rem',
+                    padding: '30px',
+                    backgroundColor: 'lightgray',
+                  }}
+                />
+              </Card>
+              <p className="text-capitalize text-center pt-3">badge and pill</p>
+            </Col>
+            <Col md={3}>
+              <Card className="" style={{ width: '13rem', height: '12rem' }}>
+                <Card.Img
+                  variant="center"
+                  src={process.env.PUBLIC_URL + '/image/comimg1.jpg'}
+                  style={{
+                    width: '13rem',
+                    height: '12rem',
+                    padding: '30px',
+                    backgroundColor: 'lightgray',
+                  }}
+                />
+              </Card>
+              <p className="text-capitalize text-center pt-3">avatar</p>
+            </Col>
+          </Row>
+          <div className="pt-5">
+            <p>Text Inputs & File Uploading.</p>
+            <Row>
+              <Col md={3}>
+                <Card className="" style={{ width: '13rem', height: '12rem' }}>
+                  <Card.Img
+                    variant="center"
+                    src={process.env.PUBLIC_URL + '/image/comimg1.jpg'}
+                    style={{
+                      width: '13rem',
+                      height: '12rem',
+                      padding: '30px',
+                      backgroundColor: 'lightgray',
+                    }}
+                  />
+                </Card>
+
+                <p className="text-capitalize text-center pt-3">text input</p>
+              </Col>
+              <Col md={3}>
+                <Card className="" style={{ width: '13rem', height: '12rem' }}>
+                  <Card.Img
+                    variant="center"
+                    src={process.env.PUBLIC_URL + '/image/comimg2.jpg'}
+                    style={{
+                      width: '13rem',
+                      height: '12rem',
+                      padding: '30px',
+                      backgroundColor: 'lightgray',
+                    }}
+                  />
+                </Card>
+                <p className="text-capitalize text-center pt-3">search input</p>
+              </Col>
+              <Col md={3}>
+                <Card className="" style={{ width: '13rem', height: '12rem' }}>
+                  <Card.Img
+                    variant="center"
+                    src={process.env.PUBLIC_URL + '/image/comimg1.jpg'}
+                    style={{
+                      width: '13rem',
+                      height: '12rem',
+                      padding: '30px',
+                      backgroundColor: 'lightgray',
+                    }}
+                  />
+                </Card>
+                <p className="text-capitalize text-center pt-3">tag input</p>
+              </Col>
+              <Col md={3}>
+                <Card className="" style={{ width: '13rem', height: '12rem' }}>
+                  <Card.Img
+                    variant="center"
+                    src={process.env.PUBLIC_URL + '/image/comimg1.jpg'}
+                    style={{
+                      width: '13rem',
+                      height: '12rem',
+                      padding: '30px',
+                      backgroundColor: 'lightgray',
+                    }}
+                  />
+                </Card>
+                <p className="text-capitalize text-center pt-3">textarea</p>
+              </Col>
+              <Col md={3}>
+                <Card className="" style={{ width: '13rem', height: '12rem' }}>
+                  <Card.Img
+                    variant="center"
+                    src={process.env.PUBLIC_URL + '/image/comimg1.jpg'}
+                    style={{
+                      width: '13rem',
+                      height: '12rem',
+                      padding: '30px',
+                      backgroundColor: 'lightgray',
+                    }}
+                  />
+                </Card>
+                <p className="text-capitalize text-center pt-3">
+                  auto complete
+                </p>
+              </Col>
+              <Col md={3}>
+                <Card className="" style={{ width: '13rem', height: '12rem' }}>
+                  <Card.Img
+                    variant="center"
+                    src={process.env.PUBLIC_URL + '/image/comimg1.jpg'}
+                    style={{
+                      width: '13rem',
+                      height: '12rem',
+                      padding: '30px',
+                      backgroundColor: 'lightgray',
+                    }}
+                  />
+                </Card>
+                <p className="text-capitalize text-center pt-3">file picker</p>
+              </Col>
+            </Row>
+          </div>
+        </div>
       </div>
     </div>
   );
